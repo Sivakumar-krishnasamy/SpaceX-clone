314e154e5906d08ae4335e8b44650c999ac81480
Sivakumar-krishnasamy <sivakumaroff5@gmail.com>
2025-02-19 06:54:26 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.getElementById("hamburger");
+const sidebar = document.getElementById("sidebar");
+const closeBtn = document.getElementById("close-btn");
+
+
+hamburger.addEventListener("click", () => {
+    sidebar.classList.add("active");
+});
+
+
+closeBtn.addEventListener("click", () => {
+    sidebar.classList.remove("active");
+});
